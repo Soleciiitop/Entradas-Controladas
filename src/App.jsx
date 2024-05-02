562aff0ac5b45426f95563580e250b225065441c
Soleciiitop <emilypatriciaaguilar@gmail.com>
2024-05-02 15:03:18 -0700

    finalized code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  // Crear un estado llamado miTexto
+  const [miTexto, setmiTexto] = useState('');
+
+
+  // Crear función handleText
+  const handleText = (event) =>{
+    //tomar el valor de la caja de entrada que podemos referenciar con 'event.target.value'
+    setmiTexto(event.target.value);
+  }
+
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div>
+          {/*Capture su texto */}
+          <input type="text" onChange={handleText} value={miTexto} />
+        </div>
+        <h3>Su Texto</h3>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {/* Mostrar el texto de la caja de entrada aquí  */}
+          {miTexto}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
